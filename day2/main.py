2b930f69b62c8adda315252c2d18e7d6a193e773
adilraja11 <marsinf@EDTS022184.>
2025-03-07 09:27:53 +0700

    feat: add custom module

---
+++ day2/main.py
@@ -0,0 +1,4 @@
+from maths import multiply, substract
+
+result = multiply(substract(4,1),4)
+print(result)
---
+++ day2/maths.py
@@ -0,0 +1,5 @@
+def multiply(x,y):
+    return x * y
+
+def substract(x,y):
+    return x - y
