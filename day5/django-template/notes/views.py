24c6c4af56e0bbea62da692065dd2e29ef2d130d
adilraja11 <marsinf@EDTS022184.>
2025-03-13 22:55:25 +0700

    feat: add day 5

--- day5/django-template/notes/views.py
+++ day5/django-template/notes/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
+    context = {
+        "name": "John Doe",
+        "age": 25,
+        "is_single": True,
+        "hobbies": ["Reading", "Gaming", "Coding"],
+        "address": {
+            "street": "Jl. Jend. Sudirman",
+            "city": "Jakarta",
+            "zip": "12345"
+        }
+    }
     # nations = ["Indonesia", "Australia", "Malaysia", "India", "Spain"]
-    nations = []
-    return render(request, "index.html", { "nations": nations })
+    # nations = []
+    return render(request, "index.html", context)
+# { "nations": nations }
