b6b3d222494afd9db3731aba8aac27470acbb2fa
adilraja11 <marsinf@EDTS022184.>
2025-03-18 14:21:09 +0700

    feat: add materials for day 5

--- day5/django-template/notes/views.py
+++ day5/django-template/notes/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
+from .models import Notes
 
 # Create your views here.
 def index_view(request):
+    notes = Notes.objects.all()
+
     context = {
-        "name": "John Doe",
-        "age": 25,
-        "is_single": True,
-        "hobbies": ["Reading", "Gaming", "Coding"],
-        "address": {
-            "street": "Jl. Jend. Sudirman",
-            "city": "Jakarta",
-            "zip": "12345"
-        }
+        "notes": notes 
     }
+
+    # context = {
+    #     "name": "John Doe",
+    #     "age": 25,
+    #     "is_single": True,
+    #     "hobbies": ["Reading", "Gaming", "Coding"],
+    #     "address": {
+    #         "street": "Jl. Jend. Sudirman",
+    #         "city": "Jakarta",
+    #         "zip": "12345"
+    #     }
+    # }
     # nations = ["Indonesia", "Australia", "Malaysia", "India", "Spain"]
     # nations = []
     return render(request, "index.html", context)
